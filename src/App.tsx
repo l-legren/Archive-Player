2c131cde560da75b82d355f26a33865aab432b12
legren <carlosleret@gmail.com>
2021-08-18 16:12:51 +0200

    boilerplatte for App with CRA, deleted unnecessary files, added styled-components

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyle from './global';
+
+const App:React.FC = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <h1>PRUEBA</h1>
+    </>
+  );
+}
+
+export default App;
