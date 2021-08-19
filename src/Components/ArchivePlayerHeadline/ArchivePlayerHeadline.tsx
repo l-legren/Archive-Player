629b844a31ef0790fe341e1bcdde0850c4b07e58
legren <carlosleret@gmail.com>
2021-08-19 11:20:44 +0200

    fetching data correctly

--- src/Components/ArchivePlayerHeadline/ArchivePlayerHeadline.tsx
+++ src/Components/ArchivePlayerHeadline/ArchivePlayerHeadline.tsx
@@ -1,4 +1,5 @@
-import React, {useState} from "react";
+import { stringify } from "querystring";
+import React, { useState } from "react";
 import {
     InputHeadline,
     HeadlineWrapper,
@@ -7,20 +8,37 @@ import {
     SectionWrapper,
     InputTitle,
     FieldWrapper,
-    InputButton
+    InputButton,
 } from "./styles";
 
-const InputSection: React.FC = () => {
+interface PlayerProfile {
+    age: string;
+    picture: string;
+    role: string;
+    team: string;
+}
 
-    const [searchPlayer, setSearchPlayer] = useState("")
+const InputSection: React.FC = () => {
+    const [searchPlayer, setSearchPlayer] = useState("");
+    const [result, setResult] = useState();
 
     const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setSearchPlayer(e.target.value)
-    }
+        setSearchPlayer(e.target.value);
+    };
 
-    const handleSubmit = () => {
-        console.log("clicking", searchPlayer)
-    }
+    const handleSubmit = async () => {
+        const profileId = await fetch(
+            `https://web-sandbox.onefootball.com/assignments/player/data/${searchPlayer}.json`
+        )
+            .then((res) => res.json())
+            .then((res) => res["profile-id"]);
+
+        const playerInfo = await fetch(
+            `https://web-sandbox.onefootball.com/assignments/player/profile/${profileId}`
+        ).then((res) => res.json());
+        console.log(playerInfo)
+        setSearchPlayer("");
+    };
 
     return (
         <SectionWrapper>
@@ -30,8 +48,13 @@ const InputSection: React.FC = () => {
             <InputWrapper>
                 <InputTitle>look for your favourite player</InputTitle>
                 <FieldWrapper>
-                    <InputField type="text" placeholder="ENTER PLAYER" value={searchPlayer} onChange={onInputChange}/>
-                    <InputButton onClick={handleSubmit} >GO!</InputButton>
+                    <InputField
+                        type="text"
+                        placeholder="ENTER PLAYER"
+                        value={searchPlayer}
+                        onChange={onInputChange}
+                    />
+                    <InputButton onClick={handleSubmit}>GO!</InputButton>
                 </FieldWrapper>
             </InputWrapper>
         </SectionWrapper>
